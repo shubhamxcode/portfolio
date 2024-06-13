a4057830631e166a424cdd2cae11eaaff0ed4ea1
shubhamxcode <shubh.varshneycode@gmail.com>
2024-06-13 14:46:58 +0530

    form

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+const boxparent1=document.querySelector('.boxparent-1')
+document.querySelector()
+const boxparent2=document.querySelector('.boxparent-2')
+const boxparent3=document.querySelector('.boxparent-3')
+
+
+let box=false
+boxparent1.addEventListener('click',function(one){
+    box=!box
+    if (box) {
+        
+    } else {
+        
+    }
+})
