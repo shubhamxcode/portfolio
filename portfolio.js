96b0431e6f028087bd64dff95562cfa1630a9687
shubhamxcode <shubh.varshneycode@gmail.com>
2024-06-14 18:34:03 +0530

    changes

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,3 @@
-const detail=document.querySelector('#detail')
+
 const developer=document.querySelector('.developer')
-const send=document.querySelector('.send')
 
-
-detail.addEventListener('click',function(){
-    document.querySelector('.send').addEventListener('click',function(){
-        console.log("hello");
-    })
-})
